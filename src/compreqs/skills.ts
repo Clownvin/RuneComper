e791bff7d436737f1894409ab8f92ba1c0d94b57
Clownvin <123clownvin@gmail.com>
2023-06-04 13:44:57 -0500

    chore: obtain level 120 status programmatically

--- src/compreqs/skills.ts
+++ src/compreqs/skills.ts
@@ -1,4 +1,4 @@
-import {SKILLS} from '../model/runescape';
+import {SKILLS, Skill} from '../model/runescape';
 import {loadWikiPage} from '../rswiki';
 import {upperFirst} from '../util';
 import {Requirement} from './requirement';
@@ -8,27 +8,26 @@ export interface SkillRequirement extends Requirement {
   maximumLevelRequirement: number;
 }
 
-const skill120s = new Set([
-  'archaeology',
-  'dungeoneering',
-  'farming',
-  'herblore',
-  'invention',
-  'slayer',
-]);
+export async function getSkillInfo(skill: Skill) {
+  const $ = await loadWikiPage(`/w/${upperFirst(skill)}/Level_up_table`);
+  const levels = $('html body div#bodyContent table.level-up-table tr th')
+    .toArray()
+    .map(e => $(e).text());
+  // console.log(levels);
+  const MIN_ROWS_120 = 124; // Extra 4 due to table headers
+  const is120 = levels.length >= MIN_ROWS_120;
+
+  return {
+    is120,
+  };
+}
 
 export async function getSkillRequirements(): Promise<SkillRequirement[]> {
   const reqs = [] as SkillRequirement[];
   for (const skill of SKILLS) {
     const page = `/w/${upperFirst(skill)}` as const;
-    // const $ = await loadWikiPage(page);
-    // const skillInfo = $(
-    //   'html body div#bodyContent table#infobox-skill tbody th'
-    // );
-
-    // console.log(skillInfo.toArray().map(e => e));
-    // console.log($.html());
-    const max = skill120s.has(skill) ? 120 : 99;
+    const {is120} = await getSkillInfo(skill);
+    const max = is120 ? 120 : 99;
     for (let level = 2; level <= max; level++) {
       const req = {
         type: 'skill',
