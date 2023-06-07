c60bd5705265b50c423f3b62e0dcd57776aef3c6
Clownvin <123clownvin@gmail.com>
2023-06-06 21:57:30 -0500

    chore: refactor AndOrMap out of Requirement

--- src/compreqs/requirement.ts
+++ src/compreqs/requirement.ts
@@ -1,5 +1,5 @@
-import {clone} from 'lodash';
 import {Skill} from '../model/runescape';
+import {AndOrElement, AndOrMap} from '../util/andOrMap';
 
 export type Type = 'quest' | 'skill' | 'achievement';
 
@@ -53,172 +53,8 @@ export function isAchievement(req: IRequirements): req is IAchievement {
   return req.type === 'achievement';
 }
 
-type RequirementsElement =
-  | IRequirements
-  | {or: Requirements}
-  | {and: Requirements};
-
-function handleAndOr<U, V>(
-  ele: RequirementsElement,
-  {
-    req,
-    and,
-    or,
-  }: {
-    req: (req: IRequirements) => U;
-    and: (reqs: Requirements) => V;
-    or: (reqs: Requirements) => V;
-  }
-) {
-  if ('and' in ele) {
-    return and(ele.and);
-  } else if ('or' in ele) {
-    return or(ele.or);
-  } else {
-    return req(ele);
-  }
-}
-
-class Requirements {
-  protected values: RequirementsElement[];
-
-  constructor(...values: readonly Readonly<RequirementsElement>[]) {
-    this.values = values.slice();
-  }
-
-  get skills(): ISkill[] {
-    return this.flatten().filter(isSkill);
-  }
-
-  get quests(): IQuestRequired[] {
-    return this.flatten().filter(isQuest);
-  }
-
-  get achievements(): IAchievement[] {
-    return this.flatten().filter(isAchievement);
-  }
-  get length() {
-    return this.values.length;
-  }
-
-  add(...values: Readonly<RequirementsElement>[]) {
-    this.values.push(...values);
-  }
-
-  *[Symbol.iterator]() {
-    for (const value of this.values) {
-      yield value;
-    }
-  }
-
-  forEach(iteratee: (req: Readonly<IRequirements>) => unknown): void {
-    for (const req of this) {
-      handleAndOr(req, {
-        and: reqs => reqs.forEach(iteratee),
-        or: reqs => reqs.forEach(iteratee),
-        req: iteratee,
-      });
-    }
-  }
-
-  reduce<T>(
-    mapper: (req: Readonly<IRequirements>) => T,
-    reducer:
-      | ((acc: T, req: T) => T)
-      | {
-          and: (acc: T, req: T) => T;
-          or: (acc: T, req: T) => T;
-        },
-    acc: T
-  ): T {
-    const start = acc;
-    const {and, or} =
-      typeof reducer === 'object' ? reducer : {and: reducer, or: reducer};
-
-    function andHelper(reqs: Requirements, acc = clone(start)): T {
-      return reqs.values.reduce(
-        (acc, req): T =>
-          handleAndOr(req, {
-            and: reqs => and(acc, andHelper(reqs)),
-            or: reqs => and(acc, orHelper(reqs)),
-            req: req => and(acc, mapper(req)),
-          }),
-        acc
-      );
-    }
-    function orHelper(reqs: Requirements, acc = clone(start)): T {
-      return reqs.values.reduce(
-        (acc, req): T =>
-          handleAndOr(req, {
-            and: reqs => or(acc, andHelper(reqs)),
-            or: reqs => or(acc, orHelper(reqs)),
-            req: req => or(acc, mapper(req)),
-          }),
-        acc
-      );
-    }
-
-    return andHelper(this);
-  }
-
-  flatten(): IRequirements[] {
-    return this.reduce(
-      req => [req],
-      (a, b) => a.concat(b),
-      [] as IRequirements[]
-    );
-  }
-
-  find<V extends IRequirements = IRequirements>(
-    predicate: (req: Readonly<IRequirements>) => req is V
-  ): V | undefined;
-  find(
-    predicate: (req: Readonly<IRequirements>) => boolean
-  ): IRequirements | undefined;
-  find(predicate: (req: Readonly<IRequirements>) => boolean) {
-    for (const value of this) {
-      const found = handleAndOr(value, {
-        and: reqs => reqs.find(predicate),
-        or: reqs => reqs.find(predicate),
-        req: req => (predicate(req) ? req : undefined),
-      });
-      if (found) {
-        return found;
-      }
-    }
-    return undefined;
-  }
-
-  splice(
-    predicate: (reqs: Readonly<RequirementsElement>) => boolean | Requirements,
-    replacement?: Requirements
-  ): RequirementsElement[] | undefined {
-    for (let i = 0; i < this.values.length; i++) {
-      const found = predicate(this.values[i]);
-      if (!found) {
-        continue;
-      }
-      replacement ??= typeof found === 'boolean' ? new Requirements() : found;
-      return this.values.splice(i, 1, ...replacement);
-    }
-    return undefined;
-  }
-
-  remove(predicate: (reqs: Readonly<RequirementsElement>) => boolean): void {
-    for (let i = 0; i < this.values.length; i++) {
-      const found = predicate(this.values[i]);
-      if (!found) {
-        continue;
-      }
-      this.values.splice(i, 1);
-      // Go back one, since we just removed index
-      i -= 1;
-    }
-  }
-}
-
 export abstract class Requirement<T extends Type = Type>
-  extends Requirements
+  extends AndOrMap<IRequirements>
   implements IRequirement
 {
   abstract readonly id: RequirementID;
@@ -235,11 +71,23 @@ export abstract class Requirement<T extends Type = Type>
     type: T;
     name: string;
     page: string;
-    requirements?: Readonly<RequirementsElement[]>;
+    requirements?: Readonly<AndOrElement<IRequirements>[]>;
   }) {
     super(...requirements);
     this.type = type;
     this.name = name;
     this.page = page;
+  }
+
+  get skills(): ISkill[] {
+    return this.flatten().filter(isSkill);
+  }
+
+  get quests(): IQuestRequired[] {
+    return this.flatten().filter(isQuest);
+  }
+
+  get achievements(): IAchievement[] {
+    return this.flatten().filter(isAchievement);
   }
 }
---
+++ src/util/andOrMap.ts
@@ -0,0 +1,187 @@
+import {clone} from 'lodash';
+import {isNonNullish} from './helpers';
+
+export type AndOrElement<T> = T | {or: AndOrMap<T>} | {and: AndOrMap<T>};
+
+export type AndOrIteratee<T, U, V> = {
+  val: (req: T) => U;
+  and: (reqs: AndOrMap<T>) => V;
+  or: (reqs: AndOrMap<T>) => V;
+};
+
+function handleAndOr<T, U, V>(
+  ele: AndOrElement<T>,
+  {val: req, and, or}: AndOrIteratee<T, U, V>
+) {
+  if (typeof ele === 'object' && isNonNullish(ele)) {
+    if ('and' in ele) {
+      return and(ele.and);
+    } else if ('or' in ele) {
+      return or(ele.or);
+    }
+  }
+  return req(ele);
+}
+
+export class AndOrMap<T> {
+  #values: AndOrElement<T>[];
+
+  constructor(...values: readonly Readonly<AndOrElement<T>>[]) {
+    this.#values = values.slice();
+  }
+
+  get length() {
+    return this.#values.length;
+  }
+
+  add(...values: AndOrElement<T>[]) {
+    this.#values.push(...values);
+  }
+
+  *[Symbol.iterator]() {
+    for (const val of this.#values) {
+      yield val;
+    }
+  }
+
+  forEach(
+    iteratee: ((val: T) => unknown) | AndOrIteratee<T, unknown, unknown>
+  ): void {
+    const defaultAnd = (reqs: AndOrMap<T>) => reqs.forEach(iteratee);
+
+    iteratee =
+      typeof iteratee === 'function'
+        ? {val: iteratee, and: defaultAnd, or: defaultAnd}
+        : iteratee;
+
+    for (const val of this) {
+      handleAndOr(val, iteratee);
+    }
+  }
+
+  map<MappedT>(
+    iteratee:
+      | ((val: T) => MappedT)
+      | AndOrIteratee<T, MappedT, AndOrElement<MappedT>>
+  ): AndOrMap<MappedT> {
+    iteratee =
+      typeof iteratee === 'function'
+        ? {
+            val: iteratee,
+            and: (values: AndOrMap<T>) => ({and: values.map(iteratee)}),
+            or: (values: AndOrMap<T>) => ({or: values.map(iteratee)}),
+          }
+        : iteratee;
+
+    const map = new AndOrMap<MappedT>();
+    for (const val of this) {
+      map.add(handleAndOr(val, iteratee));
+    }
+
+    return map;
+  }
+
+  reduce<MappedT>(
+    mapper: (val: T) => MappedT,
+    reducer:
+      | ((acc: MappedT, val: MappedT) => MappedT)
+      | {
+          and: (acc: MappedT, val: MappedT) => MappedT;
+          or: (acc: MappedT, val: MappedT) => MappedT;
+        },
+    acc: MappedT
+  ): MappedT {
+    const start = acc;
+    const {and, or} =
+      typeof reducer === 'object' ? reducer : {and: reducer, or: reducer};
+
+    function andHelper(values: AndOrMap<T>, acc = clone(start)): MappedT {
+      return values.#values.reduce(
+        (acc, val): MappedT =>
+          handleAndOr(val, {
+            and: values => and(acc, andHelper(values)),
+            or: values => and(acc, orHelper(values)),
+            val: val => and(acc, mapper(val)),
+          }),
+        acc
+      );
+    }
+    function orHelper(values: AndOrMap<T>, acc = clone(start)): MappedT {
+      return values.#values.reduce(
+        (acc, val): MappedT =>
+          handleAndOr(val, {
+            and: values => or(acc, andHelper(values)),
+            or: values => or(acc, orHelper(values)),
+            val: val => or(acc, mapper(val)),
+          }),
+        acc
+      );
+    }
+
+    return andHelper(this);
+  }
+
+  flatten(): T[] {
+    return this.reduce(
+      val => [val],
+      (a, b) => a.concat(b),
+      [] as T[]
+    );
+  }
+
+  find<FindT extends T = T>(
+    predicate: (val: T) => val is FindT
+  ): FindT | undefined;
+  find(
+    predicate: ((val: T) => boolean) | AndOrIteratee<T, boolean, T | undefined>
+  ): T | undefined;
+  find(
+    predicate: ((val: T) => boolean) | AndOrIteratee<T, boolean, T | undefined>
+  ): T | undefined {
+    const defaultAnd = (values: AndOrMap<T>) => values.find(predicate);
+
+    const iteratee =
+      typeof predicate === 'function'
+        ? {
+            val: predicate,
+            and: defaultAnd,
+            or: defaultAnd,
+          }
+        : predicate;
+
+    for (const val of this) {
+      const found = handleAndOr(val, iteratee);
+      if (found) {
+        return val as T;
+      }
+    }
+    return undefined;
+  }
+
+  splice(
+    predicate: (values: AndOrElement<T>) => boolean | AndOrMap<T>,
+    replacement?: AndOrMap<T>
+  ): AndOrElement<T>[] | undefined {
+    for (let i = 0; i < this.#values.length; i++) {
+      const found = predicate(this.#values[i]);
+      if (!found) {
+        continue;
+      }
+      replacement ??= typeof found === 'boolean' ? new AndOrMap() : found;
+      return this.#values.splice(i, 1, ...replacement);
+    }
+    return undefined;
+  }
+
+  remove(predicate: (values: AndOrElement<T>) => boolean): void {
+    for (let i = 0; i < this.#values.length; i++) {
+      const found = predicate(this.#values[i]);
+      if (!found) {
+        continue;
+      }
+      this.#values.splice(i, 1);
+      // Go back one, since we just removed index
+      i -= 1;
+    }
+  }
+}
